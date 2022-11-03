11fb452caef39bfafc03cd7a8315a8205b7310b5
KazakovGeorge <george.kazakov@yandex.ru>
2022-11-03 22:34:56 +0000

    Add example of cred file

---
+++ cred_example.py
@@ -0,0 +1,7 @@
+# !!! before using, rename this file like "cred.py" !!!
+
+mailDomain = 'outlook'
+mailLogin = 'yourmail@outlook.com'
+mailPassword = 'p@55word'
+
+emailToSend = 'mailto@domain.com'
--- mailModule.py
+++ mailModule.py
@@ -5,7 +5,7 @@ def createMail(words):
 
     message = f"Subject: EnglishWords\n\nWords:\n{words}"
 
-    smtpObj = smtplib.SMTP(f'smtp.{cred.mailDomen}.com', config.mailPort)
+    smtpObj = smtplib.SMTP(f'smtp.{cred.mailDomain}.com', config.mailPort)
     smtpObj.starttls()
     smtpObj.login(cred.mailLogin, cred.mailPassword)
     smtpObj.sendmail(cred.mailLogin, cred.emailToSend, message)
